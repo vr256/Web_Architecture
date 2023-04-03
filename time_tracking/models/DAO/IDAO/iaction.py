beac00bbb551a91ae1bd2d8f2e3d88f27963f9da
vr256 <volodyarizun4@gmail.com>
2023-04-03 14:47:46 +0300

    release: Lab 2

--- time_tracking/models/DAO/IDAO/iaction.py
+++ time_tracking/models/DAO/IDAO/iaction.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 from typing import List, Union
 from ...entities import Action
 
+
 class IAction_DAO(metaclass=ABCMeta):
     @abstractmethod
     def select_all(self, connection) -> List[Action]:
@@ -10,26 +11,26 @@ class IAction_DAO(metaclass=ABCMeta):
         '''
 
     @abstractmethod
-    def find_by_name(self, connection, name : str) -> Union[Action, bool]:
+    def find_by_name(self, connection, name: str) -> Union[Action, bool]:
         '''
         Should return action with given name 
         or False if no such action found
         '''
 
     @abstractmethod
-    def insert(self, connection, actions : List[Action]):
+    def insert(self, connection, actions: List[Action]):
         '''
         Should insert given list of actions
         '''
 
     @abstractmethod
-    def update(self, connection, actions : List[Action]):
+    def update(self, connection, actions: List[Action]):
         '''
         Should update given list of actions
         '''
 
     @abstractmethod
-    def delete(self, connection, actions : List[Action]):
+    def delete(self, connection, actions: List[Action]):
         '''
-        Should delete given list of actions 
+        Should delete given list of actions
         '''
